3030ac36322a85504bdea90f7c6d185a76ad5ad5
cacapon <takuma.tsubo@amazingengine.co.jp>
2020-07-17 11:17:37 +0900

    unittest.shでテスト実行終了時にコンテナをdownさせるように変更

--- main/container/unittest.sh
+++ main/container/unittest.sh
@@ -1,3 +1,8 @@
 docker-compose -f calc/docker-compose.yml up
+docker-compose -f calc/docker-compose.yml down
+
 docker-compose -f display/docker-compose.yml up
+docker-compose -f display/docker-compose.yml down
+
 docker-compose -f simplemessage/docker-compose.yml up
+docker-compose -f simplemessage/docker-compose.yml down
